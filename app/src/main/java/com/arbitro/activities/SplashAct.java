6e4059dc33850f3b6c3f491a53b10ede1b769f81
saqib <younissaqib@gmail.com>
2019-07-03 14:01:04 +0500

    Initial commit

---
+++ app/src/main/java/com/arbitro/activities/SplashAct.java
@@ -0,0 +1,49 @@
+package com.arbitro.activities;
+
+import android.content.Intent;
+
+import com.arbitro.MainActivity;
+import com.arbitro.R;
+import com.daimajia.androidanimations.library.Techniques;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.cnst.Flags;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
+
+public class SplashAct extends AwesomeSplash {
+    @Override
+    public void initSplash(ConfigSplash configSplash) {
+
+        /* you don't have to override every property */
+
+        //Customize Circular Reveal
+        configSplash.setBackgroundColor(R.color.colorPrimary); //any color you want form colors.xml
+        configSplash.setAnimCircularRevealDuration(2000); //int ms
+        configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);  //or Flags.REVEAL_LEFT
+        configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM); //or Flags.REVEAL_TOP
+
+        //Choose LOGO OR PATH; if you don't provide String value for path it's logo by default
+
+        //Customize Logo
+        configSplash.setLogoSplash(R.drawable.logo_150x150); //or any other drawable
+        configSplash.setAnimLogoSplashDuration(2000); //int ms
+        configSplash.setAnimLogoSplashTechnique(Techniques.BounceInDown); //choose one form Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
+
+
+
+        //Customize Title
+        configSplash.setTitleSplash("Arbitro android app");
+        configSplash.setTitleTextColor(android.R.color.white);
+        configSplash.setTitleTextSize(30f); //float value
+        configSplash.setAnimTitleDuration(3000);
+        configSplash.setAnimTitleTechnique(Techniques.FlipInX);
+       // configSplash.setTitleFont("fonts/myfont.ttf"); //provide string to your font located in assets/fonts/
+
+
+    }
+
+    @Override
+    public void animationsFinished() {
+startActivity(new Intent(this, MainActivity.class));
+finish();
+    }
+}
---
+++ app/src/main/java/com/arbitro/network/Network.java
@@ -0,0 +1,10 @@
+package com.arbitro.network;
+
+public class Network {
+    String apiKey="biuhIJ5E7miSPXqL0xNsNLpnRBXXmjtDmRAbMBK4";
+    String url="" ;
+
+
+
+
+}
